c9f0fcb88b8dacb6a124807c203d419bb6bbe236
s22203-pjatk <s22203@pjwstk.edu.pl>
2024-01-14 17:30:06 +0100

    game finished

--- Gra/main.py
+++ Gra/main.py
@@ -1,3 +1,4 @@
+import random
 
 def setup_grid():
     grid = [['_' for _ in range(5)] for _ in range(5)]
@@ -8,15 +9,47 @@ def setup_grid():
     target_pos = (4, 4)
     grid[target_pos[0]][target_pos[1]] = 'E'
 
+    for _ in range(3):
+        while True:
+            row, col = random.randint(0, 4), random.randint(0, 4)
+            if grid[row][col] == '_':
+                grid[row][col] = 'X'
+                break
+
     return grid, init_pos, target_pos
 
-def display_grid():
+def display_grid(grid):
     for grid_row in grid:
         print(' '.join(grid_row))
 
-def update_position():
+def update_position(grid, move, pos, init, end):
+    row, col = pos
+    moves = {'w': (-1, 0), 's': (1, 0), 'a': (0, -1), 'd': (0, 1)}
+    delta_row, delta_col = moves.get(move, (0, 0))
+
+    new_row, new_col = row + delta_row, col + delta_col
+    if 0 <= new_row < 5 and 0 <= new_col < 5 and grid[new_row][new_col] != 'X':
+        grid[row][col] = '_'
+        grid[new_row][new_col] = 'S'
+        return new_row, new_col
+    return row, col
 
 def game_loop():
+    grid, player_pos, end_pos = setup_grid()
+    while player_pos != end_pos:
+        print("\n")
+        display_grid(grid)
+        move = input("\nMove (W, S, A, D, q to quit): \n --> ").lower()
+        
+        if move == 'q':
+            break
+        player_pos = update_position(grid, move, player_pos, player_pos, end_pos)
+
+        if player_pos == end_pos:
+            grid[end_pos[0]][end_pos[1]] = '#'
+            display_grid(grid)
+            print("WIN!")
+
 
 if __name__ == "__main__":
     game_loop()
