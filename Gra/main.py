a3fb1dc3b7afab050c8e63695663e51d73f121fd
s22203-pjatk <s22203@pjwstk.edu.pl>
2024-01-14 16:40:53 +0100

    base

---
+++ Gra/main.py
@@ -0,0 +1,15 @@
+
+def setup_grid():
+    return
+
+def display_grid():
+    return
+
+def update_position():
+    return
+
+def game_loop():
+    return
+
+if __name__ == "__main__":
+    game_loop()
---
+++ Gra/tests.py
@@ -0,0 +1,7 @@
+import unittest
+
+class TestSimpleGame(unittest.TestCase):
+
+
+if __name__ == "__main__":
+    unittest.main()
