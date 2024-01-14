6acf5c6da1f2664a390cdb81c8fb76a9f8e249f3
s22203-pjatk <s22203@pjwstk.edu.pl>
2024-01-14 17:03:48 +0100

    v0.1

--- Gra/main.py
+++ Gra/main.py
@@ -1,15 +1,22 @@
 
 def setup_grid():
-    return
+    grid = [['_' for _ in range(5)] for _ in range(5)]
+
+    init_pos = (0, 0)
+    grid[init_pos[0]][init_pos[1]] = 'S'
+
+    target_pos = (4, 4)
+    grid[target_pos[0]][target_pos[1]] = 'E'
+
+    return grid, init_pos, target_pos
 
 def display_grid():
-    return
+    for grid_row in grid:
+        print(' '.join(grid_row))
 
 def update_position():
-    return
 
 def game_loop():
-    return
 
 if __name__ == "__main__":
     game_loop()
